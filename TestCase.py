83ea4642390f8dcc4eb8bd94354912ffc26361d3
Ismael Sanroman <ismaelsanromansanchez@gmail.com>
2022-03-15 19:58:43 +0100

    1er test (Deprecated)

--- TestCase.py
+++ TestCase.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 # from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 import time
 
 # driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
@@ -10,17 +10,34 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.maximize_window()
 driver.delete_all_cookies()
-
-driver.timeouts.page_load
-driver.timeouts.implicit_wait
-
+driver.implicitly_wait(0.5)
 driver.get("https://demoqa.com/")
 
-time.sleep(3)
+# driver.find_elements(By.XPATH, '//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]') # Revisar con JA
+driver.find_element_by_xpath('//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]').click()
+driver.find_element_by_id('item-0').click()
+time.sleep(1)
+driver.find_element_by_id('userName').clear()
+driver.find_element_by_id('userName').send_keys('Ismael')
+driver.find_element_by_id('userEmail').clear()
+driver.find_element_by_id('userEmail').send_keys('ismaelsanromansanchez@gmail.com')
+driver.find_element_by_id('currentAddress').clear()
+driver.find_element_by_id('currentAddress').send_keys('C/ República de Ecuador, '
+                                                      '6, 3º Izda, 06011, '
+                                                      'Badajoz (Badajoz)')
+driver.find_element_by_id('permanentAddress').clear()
+driver.find_element_by_id('permanentAddress').send_keys('C/ Molino de San Jerónimo, '
+                                                        '23, bajo, 10140, '
+                                                        'Guadalupe (Cáceres)')
 
-driver.find_elements(By.XPATH, '//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]')
+driver.find_element_by_id("submit").click()
 
-time.sleep(5)
+# Name:Ismael
+# Email:ismaelsanromansanchez@gmail.com
+# Current Address :C/ República de Ecuador, 6, 3º Izda, 06011, Badajoz (Badajoz)
+# Permananet Address :C/ Molino de San Jerónimo, 23, bajo, 10140, Guadalupe (Cáceres)
 
-driver.close()
-driver.quit()
+time.sleep(10)
+
+# driver.close()
+# driver.quit()
