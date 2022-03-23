945def3049d49ee1ffe5df9abba65b3182c400bd
Ismael <ismaelsanromansanchez@gmail.com>
2022-03-23 18:46:13 +0100

    Merge all branch

--- POM/Elements/RadioButtom/radioButtom_test.py
+++ POM/Elements/RadioButtom/radioButtom_test.py
@@ -1,10 +1,47 @@
 from selenium.webdriver.common.by import By
 import time
 
+
+def verify_radioButtom(driver, tag_xpath, tag_css, tag_name):
+    buttom_xpath = driver.find_element(By.XPATH, tag_xpath)
+    buttom_css = driver.find_element(By.CSS_SELECTOR, tag_css).is_displayed()
+
+    if not buttom_css:
+        print("- Está disponible para su selección")
+        if not buttom_xpath.is_selected():
+            if not buttom_xpath.is_selected():
+                buttom_xpath.click()
+
+        else:
+            print("- No es válido para seleccionar")
+    else:
+        print("- No está disponible para su selección")
+
+    if driver.find_element(By.CLASS_NAME, "text-success").text == buttom_xpath.text:
+        print("- RadioButtom, '" + tag_name + "' Seleccionado")
+    else:
+        print("--- ¡¡¡ERROR RadioButtom '" + tag_name + "'!!!")
+
 class radioButtom_test:
     def __init__(self, driver):
         self.radioButtom_flow(driver)
 
-    def radioButtom_flow(self, driver, user_data):
-        #codigo del radio buttom
-        print("hola")
+    def radioButtom_flow(self, driver):
+        driver.find_element(By.XPATH, '//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]').click()
+        driver.find_element(By.ID, 'item-2').click()
+
+        driver.implicitly_wait(3)
+
+        # ---------------------------------------------------------------------------------------
+
+        verify_radioButtom(driver, "//label[contains(text(),'Yes')]", "input[id='yesRadio']", "Yes")
+        driver.implicitly_wait(3)
+
+        verify_radioButtom(driver, "//label[contains(text(),'Impressive')]", "input[id='impressiveRadio']", "Impressive")
+        driver.implicitly_wait(3)
+
+        verify_radioButtom(driver, "//label[contains(text(),'No')]", "input[id='noRadio']", "No")
+
+        # --------------------------------------------------------------------------------------
+
+print("--- TEST CASE: 'radioButtom' ---")
