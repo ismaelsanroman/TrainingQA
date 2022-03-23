65e2fd8053ef9a14583298666550e227fe2acf86
Ismael <ismaelsanromansanchez@gmail.com>
2022-03-23 18:42:01 +0100

    TestCase radioButton finalizado

--- Training.py
+++ Training.py
@@ -1,20 +1,26 @@
 from selenium import webdriver
+import unittest
+import pytest
+import json
+import os
+
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 # from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from pyunitreport import HTMLTestRunner
+
 from POM.Elements.TextBox.textbox_test import textbox_test
 from POM.Elements.CheckBox.checkBox_test import checkBox_test
 from POM.Elements.RadioButtom.radioButtom_test import radioButtom_test
-import unittest
-import pytest
-import json
-import time
-import os
 
 
 class Training(unittest.TestCase):
+
+    """
+    @classmethod
+    def setUpClass(cls):
+    """
 
     @classmethod
     def setUp(cls):
@@ -23,28 +29,27 @@ class Training(unittest.TestCase):
         driver = cls.driver
         driver.maximize_window()
         driver.delete_all_cookies()
-        driver.implicitly_wait(8)
         driver.get("https://demoqa.com/")
+        driver.implicitly_wait(time_to_wait=10)
 
+    """
     @pytest.mark.skipif(False)
     def test1_textBox(self):
         textbox_test(self.driver, user_data=self.get_json_data())
 
-    @pytest.mark.skipif(False)
+    @pytest.mark.skipif(True)
     def test2_checkBox(self):
-        checkBox_test(self.driver, user_data=self.get_json_data())
+        checkBox_test(self.driver)
+    """
 
-    @pytest.mark.skipif(False)
+    @pytest.mark.skip
     def test3_radioButtom(self):
-        radioButtom_test(self.driver, user_data=self.get_json_data())
-
+        radioButtom_test(self.driver)
 
     @classmethod
     def tearDown(cls):
         print("\n FIN DE LOS TESTS")
-        print("ADIOS")
         cls.driver.quit()
-
 
     @staticmethod
     def get_json_data():
@@ -54,6 +59,5 @@ class Training(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    #unittest.main(testRunner=HTMLTestRunner(output=os.getcwd(), report_title="Report by Isma"))
+    # unittest.main(testRunner=HTMLTestRunner(output=os.getcwd(), report_title="Report by Ismael"))
     unittest.main()
-
