ccd8c3026050bdc3669f516618f5783583559656
Ismael <ismaelsanromansanchez@gmail.com>
2022-03-28 14:52:32 +0200

    Cofiguramos DynamicProperties

--- POM/Elements/DynamicProperties/dynamicProperties.py
+++ POM/Elements/DynamicProperties/dynamicProperties.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.common.by import By
 import time
 
+
+def dynamicProperties_flow(driver):
+    print("--- TEST CASE: 'dynamicProperties' ---")
+
+    print("--- TEST CASE Done ---")
+
+
 class dynamicProperties_test:
     def __init__(self, driver):
-        self.radioButtom_flow(driver)
-
-    def dynamicProperties_flow(self, driver):
-        print("--- TEST CASE: 'dynamicProperties' ---")
-
-        print("--- TEST CASE Done ---")
+        dynamicProperties_flow(driver)
