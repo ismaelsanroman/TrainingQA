02318a6e0b839a30891fcd1ed83efabdb7be0e4f
Ismael <ismaelsanromansanchez@gmail.com>
2022-03-28 20:15:05 +0200

    DynamicPropertiesfeature done

--- POM/Elements/DynamicProperties/dynamicProperties.py
+++ POM/Elements/DynamicProperties/dynamicProperties.py
@@ -1,9 +1,36 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 import time
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def dynamicProperties_flow(driver):
     print("--- TEST CASE: 'dynamicProperties' ---")
+    driver.find_element(By.XPATH, '//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]').click()
+    target = driver.find_element(By.ID, 'item-8')
+    target.location_once_scrolled_into_view
+    target.click()
+
+    try:
+        WebDriverWait(driver, 6).until(
+            ec.element_to_be_clickable((By.ID, 'enableAfter')))
+        print("- Elemento clicable")
+    finally:
+
+        try:
+            WebDriverWait(driver, 6).until(
+                ec.element_to_be_clickable((By.CLASS_NAME, "text-danger")))
+            print("- Color del boton modificado")
+        finally:
+
+            try:
+                WebDriverWait(driver, 6).until(
+                    ec.presence_of_element_located((By.ID, 'visibleAfter')))
+                print("- Elemento visible")
+            finally:
+                print("")
 
     print("--- TEST CASE Done ---")
 
