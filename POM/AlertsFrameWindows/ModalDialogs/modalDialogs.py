c5ee4e7fdd4a317b49b5a3e7f8c3c3f8ba2740f4
Ismael <ismaelsanromansanchez@gmail.com>
2022-03-30 04:40:25 +0200

    modalDIalogs done

--- POM/AlertsFrameWindows/ModalDialogs/modalDialogs.py
+++ POM/AlertsFrameWindows/ModalDialogs/modalDialogs.py
@@ -1,5 +1,25 @@
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def open_button(driver, tag, close_id):
+    try:
+        WebDriverWait(driver, 10).until(
+            ec.presence_of_element_located((By.ID, tag)))
+    finally:
+        ActionChains(driver).click(driver.find_element(By.ID, tag)).perform()
+        time.sleep(1)
+        num_car = len(driver.find_element(By.XPATH, "/html[1]/body[1]/div[4]/div[1]/div[1]/div[2]").text)
+        ActionChains(driver).click(driver.find_element(By.ID, close_id)).perform()
+        if num_car == 47 or num_car == 574:
+            print(f"'Button {tag}' aceptado")
+        else:
+            print(f"-- ¡¡¡ERROR!!! 'Button {tag}'")
+
+    driver.implicitly_wait(time_to_wait=3)
 
 
 def modalDialogs_flow(driver):
@@ -14,6 +34,11 @@ def modalDialogs_flow(driver):
     driver.find_element(By.XPATH, "//body/div[@id='app']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[5]").click()
 
     #  -----------------------------------------------------------------------------------------
+
+    open_button(driver, 'showSmallModal', "closeSmallModal")
+    open_button(driver, 'showLargeModal', "closeLargeModal")
+
+    time.sleep(3)
 
     print("--- TEST CASE Done ---")
 
