a87ea596c2569dfa94673893f208911d21daab86
Ismael <ismaelsanromansanchez@gmail.com>
2022-03-30 18:09:32 +0200

    framesfeature done

--- POM/AlertsFrameWindows/Frames/frames.py
+++ POM/AlertsFrameWindows/Frames/frames.py
@@ -1,5 +1,19 @@
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 import time
+
+
+def goto_frame(driver, tag, iframe):
+    try:
+        driver.switch_to.frame(iframe)
+        # WebDriverWait(driver, 1).until(ec.driver.find_element(By.ID, tag))
+        show_text = print(driver.find_element(By.ID, tag).text)
+        print(f"'{iframe}' muestra el siguiente contenido: '{show_text}'")
+        driver.switch_to.default_content()
+    except TimeoutException:
+        pass
 
 
 def frames_flow(driver):
@@ -12,8 +26,13 @@ def frames_flow(driver):
     target.location_once_scrolled_into_view
     target.click()
     driver.find_element(By.XPATH, "//body/div[@id='app']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/ul[1]/li[3]").click()
-
+    time.sleep(1)
     #  -----------------------------------------------------------------------------------------
+
+    goto_frame(driver, 'sampleHeading', 'frame1')
+    goto_frame(driver, 'sampleHeading', 'frame2')
+
+    time.sleep(3)
 
     print("--- TEST CASE Done ---")
 
